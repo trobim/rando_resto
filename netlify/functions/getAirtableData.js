036175bde506016bfb7df611ac4e33057286405f
fabio <trobim@gmail.com>
2024-02-22 23:58:31 +0100

    api tests

--- netlify/functions/getAirtableData.js
+++ netlify/functions/getAirtableData.js
@@ -8,7 +8,7 @@ exports.handler = async function(event) {
     const { data } = await axios.get(URL, {
       headers: {
         // Replace 'YOUR_PERSONAL_ACCESS_TOKEN' with your actual Airtable API key
-        Authorization: 'Bearer ${process.env.AIRTABLE_API_KEY}'
+        Authorization: 'Bearer pataeH8kJj5GBV6HR.ee9f5dd8ef23e8ab741c753d28608f40fbd75efe17da12fdaa792d459d5e3f19'
       }
     });
     return {
