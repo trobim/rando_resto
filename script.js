d3c77d901f232fa105f6110c94e6e8284dc6c252
fabio <trobim@gmail.com>
2024-02-23 00:55:21 +0100

    11

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.getElementById('fetch-data').addEventListener('click', function() {
+document.getElementById('load-data').addEventListener('click', function() {
     const apiKey = 'pataeH8kJj5GBV6HR.ee9f5dd8ef23e8ab741c753d28608f40fbd75efe17da12fdaa792d459d5e3f19';
-    const endpoint = 'https://api.airtable.com/v0/appmncgSEVQ1n0qQ0/tbl6jy5YE156eCJoq?api_key=' + apiKey;
+    const endpoint = 'https://api.airtable.com/v0/appmncgSEVQ1n0qQ0/tbl6jy5YE156eCJoq';
+    const headers = {
+        Authorization: 'Bearer ' + apiKey
+    };
 
-    fetch(endpoint)
+    fetch(endpoint, {headers})
     .then(response => response.json())
     .then(data => {
         const container = document.getElementById('data-container');
