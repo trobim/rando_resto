fa9db8d679e10ed7a97b38a030966db255c9369b
fabio <trobim@gmail.com>
2024-02-25 20:35:39 +0100

    netlify cors

--- netlify/functions/fetch-restaurants.js
+++ netlify/functions/fetch-restaurants.js
@@ -30,6 +30,11 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allows requests from any origin
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET"
+      },
       body: JSON.stringify(restaurants)
     };
   } catch (error) {
